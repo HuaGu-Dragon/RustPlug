7c7bf6a64a8ed8fc9284dca7f46b4d25e08b627a
俺不叫化咕龙 <1801943622@qq.com>
2025-12-02 19:58:59 +0800

    Success to call the dll.

--- hello_world/src/lib.rs
+++ hello_world/src/lib.rs
@@ -1,3 +1,4 @@
+#[unsafe(no_mangle)]
 pub extern "C" fn hello_world() {
     println!("Hello, World!");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,31 @@
-fn main() {}
+use std::{ffi::OsStr, os::windows::ffi::OsStrExt};
+
+use winapi::um::libloaderapi::{FreeLibrary, GetProcAddress, LoadLibraryW};
+
+fn main() {
+    let dll_path = "hello_world.dll";
+
+    let path: Vec<_> = OsStr::new(dll_path).encode_wide().chain(Some(0)).collect();
+    let handle = unsafe { LoadLibraryW(path.as_ptr()) };
+
+    if handle.is_null() {
+        eprintln!(
+            "Failed to load DLL, error: {:?}",
+            std::io::Error::last_os_error()
+        );
+        return;
+    }
+    let addr = unsafe { GetProcAddress(handle, c"hello_world".as_ptr()) };
+
+    if addr.is_null() {
+        eprintln!(
+            "Failed to find function, error: {:?}",
+            std::io::Error::last_os_error()
+        );
+    }
+
+    let hello_world: fn() = unsafe { std::mem::transmute(addr) };
+    hello_world();
+
+    unsafe { FreeLibrary(handle) };
+}
