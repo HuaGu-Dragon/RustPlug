dd7759bf056d95a3d5638eb024addd949748556e
俺不叫化咕龙 <1801943622@qq.com>
2025-12-04 09:30:19 +0800

    Add `math.dll`.

---
+++ math/src/lib.rs
@@ -0,0 +1,26 @@
+#[unsafe(no_mangle)]
+pub fn add(left: i32, right: i32) -> i32 {
+    let res = left + right;
+    println!("{left} + {right} = {res}");
+    res
+}
+
+#[unsafe(no_mangle)]
+pub fn sub(left: i32, right: i32) -> i32 {
+    let res = left - right;
+    println!("{left} - {right} = {res}");
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+        let result = sub(3, 1);
+        assert_eq!(result, 2);
+    }
+}
